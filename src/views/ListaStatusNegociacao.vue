67488cd4607c1a77ec7ccdde336d48d7f0a6570d
benhur.suita <benhur.suita@gmail.com>
2025-12-09 15:59:51 -0300

    - substituido toolbarHeight pela medida 64px para que a barra preta fique abaixo da barra azul;

--- src/views/ListaStatusNegociacao.vue
+++ src/views/ListaStatusNegociacao.vue
@@ -15,7 +15,7 @@
                     'border-top-left-radius': '12px',
                     'border-top-right-radius': '8px',
                     'position': 'fixed',
-                    'top': toolbarHeight + 'px',
+                    'top': '64px',
                     'left': '260px', 
                     'width': '84.7%',
                     'right': '0',
@@ -193,7 +193,7 @@
 
     // Cria os headers da tabela
     const headersStatusNegociacao = [
-        { title: 'Funções', value: 'funcoes', sortable: false },
+        { title: 'Editar', value: 'funcoes', sortable: false },
         { title: 'Código', value: 'idStatusNegociacao', sortable: true },
         { title: 'Status Negocição', value: 'nome', sortable: true },
         { title: 'Condição', value: 'ativo', sortable: true }        
