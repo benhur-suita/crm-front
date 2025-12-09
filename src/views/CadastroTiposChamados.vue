8459d7b00ba14e62911235059247e77b768a29bb
benhur.suita <benhur.suita@gmail.com>
2025-12-09 15:57:36 -0300

    - substituido toolbarHeight pela medida 64px para que a barra preta fique abaixo da barra azul; - Retirada a conversão de true do campo ativo e passada para o backend;

--- src/views/CadastroTiposChamados.vue
+++ src/views/CadastroTiposChamados.vue
@@ -15,7 +15,7 @@
                     'border-top-left-radius': '12px',
                     'border-top-right-radius': '8px',
                     'position': 'fixed',
-                    'top': toolbarHeight + 'px',
+                    'top': '64px',
                     'left': '260px', 
                     'width': '84.7%',
                     'right': '0',
@@ -212,10 +212,11 @@
                 
                 if (respostaApi) {
 
-                    // Converte os campos TINYINT(1) para booleanos
-                    respostaApi.ativo = respostaApi.ativo === 1;
+                    // Atribui os dados retornados da API para a variável dadosTipoChamados
+                    dadosTipoChamados.value = respostaApi;
 
-                    dadosTipoChamados.value = respostaApi;                   
+                    // Converte os campos TINYINT(1) para booleanos
+                    dadosTipoChamados.value.ativo = dadosTipoChamados.value.ativo === 1;                    
                 }
 
             } catch (erro) {
